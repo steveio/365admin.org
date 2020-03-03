c210d2209a344726f8a672a4364a58b382e84ff8
Steve Edwards <steve@oneworld365.org>
2020-03-03 19:16:55 +0000

    Include cache class

--- article_opt_ajax.php
+++ article_opt_ajax.php
@@ -18,6 +18,7 @@ require_once("./classes/db_pgsql.class.php");
 require_once("./classes/logger.php");
 require_once("./classes/template.class.php");
 require_once("./classes/article.class.php");
+require_once("./classes/cache.class.php");
 
 $db = new db($dsn,$debug = false);
 
