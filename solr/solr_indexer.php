65059ff31031811451204d13127717e23347ee30
Steve Edwards <steve@oneworld365.org>
2023-12-19 16:19:18 +0000

    PHP display_errors = 0

--- solr/solr_indexer.php
+++ solr/solr_indexer.php
@@ -20,7 +20,7 @@
  * 
  */
 
-ini_set('display_errors',1);
+ini_set('display_errors',0);
 ini_set('log_errors', 1);
 ini_set('error_log', '/www/vhosts/365admin.org/logs/365admin_indexer.log');
 error_reporting(E_ALL & ~E_NOTICE & ~ E_STRICT);
