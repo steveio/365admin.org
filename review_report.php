d0df1d70cf898bcf892e28111e3609a568a4b5fb
steveio.com <info@steveio.com>
2024-01-07 14:47:21 +0000

    Responsive UI

--- review_report.php
+++ review_report.php
@@ -78,87 +78,96 @@ print $oHeader->Render();
 
 ?>
 
-<!-- BEGIN Page Content Container -->
-<div class="">
-<div class="">
-
+<div class="container">
+<div class="align-items-center justify-content-center">
 
 <form name="report_filter" enctype="multipart/form-data" action="" method="POST">
 
-<label for="daterange">Date range:</label>
-<input type="text" name="daterange" value="<?= $strDateRange; ?>" />
+<div class="row my-3">
 
-<label for="daterange">Select all:</label>
-<input type="checkbox" id="" name="report_all" <?= (isset($_REQUEST['report_all'])) ? "checked" : ""; ?>/>
+	<div class="col-12">
 
-<label for="daterange">By status:</label>
-<select id="" name="report_status">
-	<option value="ALL">ALL</option>
-	<option value="0" <?= ($_REQUEST['report_status'] == "0") ? "selected" : ""; ?>>PENDING</option>
-	<option value="1" <?= ($_REQUEST['report_status'] == "1") ? "selected" : ""; ?>>APPROVED</option>
-	<option value="2"<?= ($_REQUEST['report_status'] == "2") ? "selected" : ""; ?>>REJECTED</option>
-</select>
-<input type="submit" name="report_filter" value="go" onClick="this.form.submit()" />
-
+        <label for="daterange">Date range:</label>
+        <input type="text" name="daterange" value="<?= $strDateRange; ?>" />
+        
+        <label for="daterange">Select all:</label>
+        <input type="checkbox" id="" name="report_all" <?= (isset($_REQUEST['report_all'])) ? "checked" : ""; ?>/>
+        
+        <label for="daterange">By status:</label>
+        <select id="" name="report_status">
+        	<option value="ALL">ALL</option>
+        	<option value="0" <?= ($_REQUEST['report_status'] == "0") ? "selected" : ""; ?>>PENDING</option>
+        	<option value="1" <?= ($_REQUEST['report_status'] == "1") ? "selected" : ""; ?>>APPROVED</option>
+        	<option value="2"<?= ($_REQUEST['report_status'] == "2") ? "selected" : ""; ?>>REJECTED</option>
+        </select>
+		<button class="btn btn-primary rounded-pill px-3" type="button" name="report_filter" value="go" onClick="this.form.submit()">submit</button>
+	</div>
+</div>
 	
 <? if (strlen($strMessage) >= 1) { ?>
-    <div style="font-weight: bold; margin: 20px 0px 20px 0px;">
+    <div class="alert alert-success" role="alert">
     <h3><img src="/images/icon_green_tick.png" border="0" /><?= $strMessage; ?></h3>
     </div>
 <?php } ?>
 
-
-<div id='' class="span12" style='margin-top: 40px;'>
+<div class="row my-3">
 
 <h1>Reviews</h1>
 
+</div>
 
+<div class="row">
 <div style="clear: both;">
-<div style="float: right; margin-bottom: 20px;">
+    <div style="float: right;">
 		<select name="bulk_action">
 			<option value="">select</option>
 			<option value="approve">approve selected</option>
 			<option value="reject">reject selected</option>
 		</select>
 		<input type="button" name="go_batch" value="go" onClick="this.form.submit()" />
+	</div>
 </div>
 </div>
 
-<table id="report" class="display" cellspacing="2" cellpadding="4" border="0">
 
+
+<div class="row my-3">
+
+<table id="report" class="display" cellspacing="2" cellpadding="4" border="0" class="table table-striped">	
+    
 	<thead>
-	<tr><?
-$aRow = $aReport[0];
-
-$aKeys = array_keys($aRow);
-foreach($aKeys as $idx => $key) { ?>
-	<th><?= $key; ?></th><? 
-} ?>
-	<th>edit</th>
-	<th>approve</th>
-	<th>reject</th>
-	<th>bulk</th>	
-	</tr>
+    	<tr><?
+    $aRow = $aReport[0];
+    
+    $aKeys = array_keys($aRow);
+    foreach($aKeys as $idx => $key) { ?>
+    	<th><?= $key; ?></th><? 
+    } ?>
+    	<th>edit</th>
+    	<th>approve</th>
+    	<th>reject</th>
+    	<th>bulk</th>	
+    	</tr>
 	</thead>
 	
 	<tbody>
-
-<?
-foreach($aReport as $aRow) { ?>
-	<tr><?php 
-    foreach($aRow as $key => $value)
-    { ?>
-		<td id="<?= $key; ?>" valign="top"><?= $value; ?></td><? 
+    
+    <?
+    foreach($aReport as $aRow) { ?>
+    	<tr><?php 
+        foreach($aRow as $key => $value)
+        { ?>
+    		<td id="<?= $key; ?>" valign="top"><?= $value; ?></td><? 
+        } ?>
+    	<td width="20px"><a href="../edit_review/?&id=<?= $aRow['post_id'] ?>" target="_new">edit</a></td>
+    	<td width="20px"><input type="submit" onclick="javascript: return confirm('Are you sure you wish to approve this review?');" name="enq_<?= $aRow['post_id'] ?>_approve" value="approve" /></td>
+    	<td width="20px"><input type="submit" onclick="javascript: return confirm('Are you sure you wish to reject this review?');" name="enq_<?= $aRow['post_id'] ?>_reject" value="reject" /></td>
+    	<td width="20px" valign="top"><input type="checkbox" id="enq_<?= $aRow['post_id'] ?>" name="enq_<?= $aRow['post_id'] ?>" value="approve" /></td>
+    
+    	</tr><?
     } ?>
-	<td width="20px"><a href="../edit_review/?&id=<?= $aRow['post_id'] ?>" target="_new">edit</a></td>
-	<td width="20px"><input type="submit" onclick="javascript: return confirm('Are you sure you wish to approve this review?');" name="enq_<?= $aRow['post_id'] ?>_approve" value="approve" /></td>
-	<td width="20px"><input type="submit" onclick="javascript: return confirm('Are you sure you wish to reject this review?');" name="enq_<?= $aRow['post_id'] ?>_reject" value="reject" /></td>
-	<td width="20px" valign="top"><input type="checkbox" id="enq_<?= $aRow['post_id'] ?>" name="enq_<?= $aRow['post_id'] ?>" value="approve" /></td>
-
-	</tr><?
-} ?>
-</tbody>
-</table>
+    </tbody>
+    </table>
 
 </form>
 
@@ -182,7 +191,7 @@ $(document).ready(function() {
 
     $('#report').DataTable({
     	"pageLength": 100,
-    	"bSort" : false
+    	"bSort" : true
     });
 
 });
@@ -191,7 +200,6 @@ $(document).ready(function() {
 
 </div>
 </div>
-<!-- END Page Content Container -->
 
 <?
 print $oFooter->Render();
