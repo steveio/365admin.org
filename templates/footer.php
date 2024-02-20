de765cda0f545d1a6127f41c766c62a4958802fb
steveio.com <info@steveio.com>
2024-02-20 13:09:10 +0000

    Footer icons

--- templates/footer.php
+++ templates/footer.php
@@ -21,12 +21,22 @@
         <div class="">
             <div class="footer-icon">
                 <h4>Follow Our Social Media Pages</h4>
-		<div class="">
-                <p><a href="http://www.facebook.com/oneworld365" target="_blank"><img alt="One World 365 Facebook" src="http://www.oneworld365.org/img/101/facebook_84.png" /></a>&nbsp;&nbsp;<a href="http://www.instagram.com/oneworld365" target="_blank"><img alt="One World 365 Instagram" src="http://www.oneworld365.org/img/101/instagram_84.png" /></a>&nbsp;&nbsp;<a href="http://www.twitter.com/oneworld365" target="_blank"><img alt="One World 365 Twitter" src="http://www.oneworld365.org/img/101/twitter_84.jpg" /></a>&nbsp;&nbsp;<a href="http://www.pinterest.com/oneworld365" target="_blank"><img alt="One World 365 Pinterest" src="http://www.oneworld365.org/img/101/pinterest_84.png" /></a></p>
-		</div>
+				<div class="col-12 p-3">
+                <a href="http://www.facebook.com/oneworld365" target="_blank"><img alt="One World 365 Facebook" src="http://www.oneworld365.org/img/101/facebook_84_50.png" /></a>
+                <a href="http://www.instagram.com/oneworld365" target="_blank"><img alt="One World 365 Instagram" src="http://www.oneworld365.org/img/101/instagram_84_50.png" /></a>
+                <a href="http://www.twitter.com/oneworld365" target="_blank"><img alt="One World 365 Twitter" src="http://www.oneworld365.org/img/101/twitter_84_50.jpg" /></a>
+                <a href="http://www.pinterest.com/oneworld365" target="_blank"><img alt="One World 365 Pinterest" src="http://www.oneworld365.org/img/101/pinterest_84_50.png" /></a>
+				</div>
             
+            	<div class="col-12 p-3">
                 <h4>Partners &amp; Featured In</h4>
-            	<img alt="FCO" src="http://www.oneworld365.org/img/101/fco_84.png" />&nbsp;&nbsp;<img alt="BBC Worldwide" src="http://www.oneworld365.org/img/101/bbc_worldwide_84.png" />&nbsp; <img alt="USA Today" src="http://www.oneworld365.org/img/101/usa_today_84.png" />&nbsp;&nbsp;<img alt="Rough Guides" src="http://www.oneworld365.org/img/101/rough_guides_84.jpg" />&nbsp;&nbsp;<img alt="Lonely Planet" src="http://www.oneworld365.org/img/101/lonely_planet_84.png" />&nbsp;&nbsp;<img alt="National Geographic" src="http://www.oneworld365.org/img/101/Natgeo_84.png" />
+            	<img alt="FCO" src="http://www.oneworld365.org/img/101/fco_84_50.png" />
+            	<img alt="BBC Worldwide" src="http://www.oneworld365.org/img/101/bbc_worldwide_84_50.png" /> 
+            	<img alt="USA Today" src="http://www.oneworld365.org/img/101/usa_today_84_50.png" />
+            	<img alt="Rough Guides" src="http://www.oneworld365.org/img/101/rough_guides_84_50.jpg" />
+            	<img alt="Lonely Planet" src="http://www.oneworld365.org/img/101/lonely_planet_84_50.png" />
+            	<img alt="National Geographic" src="http://www.oneworld365.org/img/101/Natgeo_84_50.png" />
+            	</div>
             </div>
         </div>
     </div>
