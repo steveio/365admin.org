0b361cb3076d7feb785cc8787832ac87300f8b29
Steve Edwards <steve@oneworld365.org>
2024-03-08 11:46:42 +0000

    Sysadmin Shell Scripts

--- scripts/article_map_opt.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE article_map_opts ADD COLUMN opt_gads boolean default true;
-ALTER TABLE article_map_opts ADD COLUMN opt_ads boolean default true;
-
-update article_map_opts set opt_gads = true;
-update article_map_opts set opt_ads = true;
